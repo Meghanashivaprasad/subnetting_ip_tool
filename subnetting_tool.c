fc05bf6af7469c5a4217fcd8b51425bce75259f5
Meghanashivaprasad <123771659+Meghanashivaprasad@users.noreply.github.com>
2024-01-05 00:53:45 -0800

    added subnetting_tool.h

--- subnetting_tool.c
+++ subnetting_tool.c
@@ -3,16 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include "subnetting_tool.h"
 
 
-char* calculate_net_id(char* ip, char* net_mask);
-char* calculate_broadcast_address(char* ip, char* net_mask);
-int count_ones(char* net_mask);
-int max_hosts(int ones);
-char* makeNetMask(int ones);
-void splitIPAddress(char *ipAddres, char* ipParts[]);
-char* fromDecimalToBinary(int decimal);
-char* binaryToDotDecimal(char* binaryNumber);
 
 
 char* binaryToDotDecimal(char* binaryNumber) {
